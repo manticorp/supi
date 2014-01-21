58efc0eae19481b988d2e52098f800b83e50eb10
Harry Mustoe-Playfair <manticorp1234@gmail.com>
2014-01-21 12:41:23 +0000

    Initial version

--- getWeather.php
+++ getWeather.php
@@ -2,13 +2,9 @@
 include('forecast.io.php');
 header('Content-type: application/json');
 
-<<<<<<< HEAD
 $config = parse_ini_file("config.ini");
 
 $api_key = $config["forecastioAPIkey"];
-=======
-$api_key = "6b80c5e93b04f843aab9f440bb12b7e5";
->>>>>>> Initial project version.
 
 $latitude   = $_REQUEST["latitude"];
 $longitude  = $_REQUEST["longitude"];
